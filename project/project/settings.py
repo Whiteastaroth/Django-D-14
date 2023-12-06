11eb262608e79e520505dd07fd3155cbf5f92ba0
Whiteastaroth <125083144+Whiteastaroth@users.noreply.github.com>
2023-12-06 16:28:06 +0300

    Add files via upload

--- project/project/settings.py
+++ project/project/settings.py
@@ -35,6 +35,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'modeltranslation',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -52,6 +54,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.yandex',
 
     'django_apscheduler',
+
+
 
 ]
 
