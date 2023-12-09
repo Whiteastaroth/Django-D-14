d72c622d7a9b61cd2505d187a10d256bda7a308e
Whiteastaroth <125083144+Whiteastaroth@users.noreply.github.com>
2023-12-10 00:40:34 +0300

    Add files via upload

--- project/news/filters.py
+++ project/news/filters.py
@@ -1,15 +1,20 @@
 from django_filters import FilterSet, ModelChoiceFilter, CharFilter, DateFilter
-from django.forms import DateInput
+from django.forms import SelectDateWidget
 from .models import Category
+from django.utils.translation import gettext as _, gettext_lazy
 
 
 class NewFilter(FilterSet):
     slug = ModelChoiceFilter(field_name= 'category__title',
                              queryset = Category.objects.all(),
-                             label = 'Категория',
-                             empty_label = 'Любой'
+                             label = gettext_lazy('Категория'),
+                             empty_label = gettext_lazy('Любой')
                              )
 
     title = CharFilter(lookup_expr='contains',)
 
-    date = DateFilter(field_name='data', lookup_expr='gt', label='Дата', widget=DateInput(attrs={'type': 'date'},))
+    date = DateFilter(field_name='data',
+                      lookup_expr='gt',
+                      label=gettext_lazy('Дата'),
+                      widget=SelectDateWidget(attrs={'type': 'date'},)
+                      )
