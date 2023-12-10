2aca4c41f5b5e84112b644b4097cc1a05fd4708e
Whiteastaroth <125083144+Whiteastaroth@users.noreply.github.com>
2023-12-10 14:25:29 +0300

    Add files via upload

--- project/news/forms.py
+++ project/news/forms.py
@@ -3,20 +3,23 @@ from django.forms import ModelForm
 from django.core.exceptions import ValidationError
 from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
 
+from django.utils.translation import gettext as _, gettext_lazy
+
+
 class NewForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['category'].empty_label = 'вы не выбрали категорию'
+        self.fields['category'].empty_label = _('вы не выбрали категорию')
 
     class Meta:
         model = New
         fields = ['title', 'text',  'category']
 
         widgets = {
-            'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Название публикации'}),
-            'datе': DateTimeInput(attrs={'type': 'date', 'class': 'form-control', 'placeholder': 'Дата публикации'}),
-            'text': Textarea(attrs={'class': 'form-control', 'placeholder': 'Текст публикации'}),
-            'category__title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Категория'})
+            'title': TextInput(attrs={'class': 'form-control', 'placeholder': gettext_lazy('Название публикации')}),
+            'datе': DateTimeInput(attrs={'type': 'date', 'class': 'form-control', 'placeholder': gettext_lazy('Дата публикации')}),
+            'text': Textarea(attrs={'class': 'form-control', 'placeholder': gettext_lazy('Текст публикации')}),
+            'category__title': TextInput(attrs={'class': 'form-control', 'placeholder': gettext_lazy('Категория')})
         }
 
 
@@ -25,7 +28,7 @@ def clean(self):
     text = cleaned_data.get("text")
     if text is not None and len(text) < 20:
         raise ValidationError({
-            "text": "Описание не может быть менее 20 символов."
+            "text": gettext_lazy("Описание не может быть менее 20 символов.")
         })
 
     return text
